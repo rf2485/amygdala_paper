232119c926ecc316b9d30006b22a6f65199b3071
Ryn Flaherty <rf2485@nyu.edu>
2025-03-04 13:07:41 -0500

    removing wm analyses

--- 3a_qc_images.sh
+++ 3a_qc_images.sh
@@ -37,28 +37,6 @@ if [ ! -f $t1dir/group_qc/recon/index.html ]; then
 	echo '</BODY></HTML>' >> $t1dir/group_qc/recon/index.html
 fi
 
-#generate QC webpage for JHU in T1 subject space
-if [ ! -f $t1dir/group_qc/jhu2fs/index.html ]; then
-	mkdir -p $t1dir/group_qc/jhu2fs
-	echo '<HTML><TITLE>jhu2fs</TITLE><BODY BGCOLOR="#aaaaff">' > $t1dir/group_qc/jhu2fs/index.html
-	for subj in "${subj_list[@]}"; do
-		if [ ! -f $t1dir/group_qc/jhu2fs/$subj.png ]; then
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 102 128 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grota.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 128 128 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grotb.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 154 128 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grotc.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 128 128 102 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grotd.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 128 128 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grote.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 128 128 154 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grotf.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 128 102 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/grotg.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 128 128 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/groth.png
-			freeview -v $t1dir/$subj/mri/T1.mgz $t1dir/$subj/mri/jhu2fs.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 128 154 128 -nocursor -screenshot $t1dir/group_qc/jhu2fs/groti.png
-			pngappend $t1dir/group_qc/jhu2fs/grota.png + $t1dir/group_qc/jhu2fs/grotb.png + $t1dir/group_qc/jhu2fs/grotc.png + $t1dir/group_qc/jhu2fs/grotd.png + $t1dir/group_qc/jhu2fs/grote.png + $t1dir/group_qc/jhu2fs/grotf.png + $t1dir/group_qc/jhu2fs/grotg.png + $t1dir/group_qc/jhu2fs/groth.png + $t1dir/group_qc/jhu2fs/groti.png $t1dir/group_qc/jhu2fs/$subj.png
-			echo '<a href="'${subj}'.png"><img src="'${subj}'.png" WIDTH='1000' >' ${subj}'</a><br>' >> $t1dir/group_qc/jhu2fs/index.html
-		fi
-	done
-	echo '</BODY></HTML>' >> $t1dir/group_qc/jhu2fs/index.html
-fi
-
 #generate QC webpages for diffusion processing
 mkdir -p $dwidir/group_qc/intermediate_nifti
 mkdir -p $dwidir/group_qc/metrics
@@ -121,28 +99,6 @@ if [ ! -f $t1dir/group_qc/aparc+aseg2diff/index.html ]; then
 	echo '</BODY></HTML>' >> $t1dir/group_qc/aparc+aseg2diff/index.html
 fi
 
-#generate QC webpage for jhu2diff
-if [ ! -f $t1dir/group_qc/jhu2diff/index.html ]; then
-	mkdir -p $t1dir/group_qc/jhu2diff
-	echo '<HTML><TITLE>jhu2diff</TITLE><BODY BGCOLOR="#aaaaff">' > $t1dir/group_qc/jhu2diff/index.html
-	for subj in "${subj_list[@]}"; do
-		if [ ! -f $t1dir/group_qc/jhu2diff/$subj.png ]; then
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 38 48 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grota.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 48 48 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grotb.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 58 48 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grotc.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 48 38 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grotd.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 48 48 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grote.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 48 58 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grotf.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 48 48 26 -nocursor -screenshot $t1dir/group_qc/jhu2diff/grotg.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 48 48 33 -nocursor -screenshot $t1dir/group_qc/jhu2diff/groth.png
-			freeview -v $t1dir/$subj/diffusion/B0.nii $t1dir/$subj/diffusion/jhu2diff_NODDI_mask.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 48 48 40 -nocursor -screenshot $t1dir/group_qc/jhu2diff/groti.png
-			pngappend $t1dir/group_qc/jhu2diff/grota.png + $t1dir/group_qc/jhu2diff/grotb.png + $t1dir/group_qc/jhu2diff/grotc.png + $t1dir/group_qc/jhu2diff/grotd.png + $t1dir/group_qc/jhu2diff/grote.png + $t1dir/group_qc/jhu2diff/grotf.png + $t1dir/group_qc/jhu2diff/grotg.png + $t1dir/group_qc/jhu2diff/groth.png + $t1dir/group_qc/jhu2diff/groti.png $t1dir/group_qc/jhu2diff/$subj.png
-			echo '<a href="'${subj}'.png"><img src="'${subj}'.png" WIDTH='1000' >' ${subj}'</a><br>' >> $t1dir/group_qc/jhu2diff/index.html
-		fi
-	done
-	echo '</BODY></HTML>' >> $t1dir/group_qc/jhu2diff/index.html
-fi
-
 #generate QC webpages for MTI processing
 if [ ! -f $t1dir/group_qc/1_mti_reg/index.html ]; then
 	mkdir -p $t1dir/group_qc/1_mti_reg
@@ -195,50 +151,3 @@ if [ ! -f $t1dir/group_qc/aparc+aseg2mtr/index.html ]; then
 	done
 	echo '</BODY></HTML>' >> group_qc/aparc+aseg2mtr/index.html
 fi
-
-#generate QC webpage for jhu2mtr
-if [ ! -f $t1dir/group_qc/jhu2mtr/index.html ]; then
-	mkdir -p $t1dir/group_qc/jhu2mtr
-	echo '<HTML><TITLE>jhu2mtr</TITLE><BODY BGCOLOR="#aaaaff">' > $t1dir/group_qc/jhu2mtr/index.html
-	for subj in "${subj_list[@]}"; do
-		if [ ! -f $t1dir/group_qc/jhu2mtr/$subj.png ]; then
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 42 64 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grota.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 52 64 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grotb.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'x' -slice 62 64 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grotc.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 52 51 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grotd.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 52 64 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grote.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'y' -slice 52 77 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grotf.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 52 64 51 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/grotg.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 52 64 64 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/groth.png
-			freeview -v $t1dir/$subj/mti/mtr.nii.gz $t1dir/$subj/mti/jhu2mtr.mgz:colormap=lut:opacity=0.2 -viewport 'z' -slice 52 64 77 -nocursor -screenshot $t1dir/group_qc/jhu2mtr/groti.png
-			pngappend $t1dir/group_qc/jhu2mtr/grota.png + $t1dir/group_qc/jhu2mtr/grotb.png + $t1dir/group_qc/jhu2mtr/grotc.png + $t1dir/group_qc/jhu2mtr/grotd.png + $t1dir/group_qc/jhu2mtr/grote.png + $t1dir/group_qc/jhu2mtr/grotf.png + $t1dir/group_qc/jhu2mtr/grotg.png + $t1dir/group_qc/jhu2mtr/groth.png + $t1dir/group_qc/jhu2mtr/groti.png $t1dir/group_qc/jhu2mtr/$subj.png
-			echo '<a href="'${subj}'.png"><img src="'${subj}'.png" WIDTH='1000' >' ${subj}'</a><br>' >> $t1dir/group_qc/jhu2mtr/index.html
-		fi
-	done
-	echo '</BODY></HTML>' >> $t1dir/group_qc/jhu2mtr/index.html
-fi
-
-#generate QC webpage for mtr2diff
-subj_list=$(cut -f1 -d$'\t' $projectdir/mti_over_55.tsv)
-subj_list=($subj_list)
-
-if [ ! -f $t1dir/group_qc/mtr2diff/index.html ]; then
-	mkdir -p $t1dir/group_qc/mtr2diff
-	for subj in "${subj_list[@]}"; do
-		echo $subj
-		cp $t1dir/$subj/diffusion/B0.nii $t1dir/group_qc/mtr2diff/${subj}_1_B0.nii
-		cp $t1dir/$subj/diffusion/mtr2diff.nii $t1dir/group_qc/mtr2diff/${subj}_2_mtr2diff.nii
-	done
-	cd $t1dir
-	slicesdir -o group_qc/mtr2diff/*.nii
-	rm -rf $t1dir/group_qc/mtr2diff
-	mv slicesdir $t1dir/group_qc/mtr2diff
-fi
-
-#generate QC webpage for g-ratio
-if [ ! -f $t1dir/group_qc/g_ratio/index.html ]; then
-	slicesdir $t1dir/*/diffusion/g_ratio.nii
-	rm -rf $t1dir/group_qc/g_ratio
-	mv slicesdir $t1dir/group_qc/g_ratio
-fi
-
